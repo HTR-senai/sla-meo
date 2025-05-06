863e84060ea9d3efebc9123e4a3bee4bcbae9a21
HTR-senai <0001138366@senaimgaluno.com.br>
2025-05-06 14:58:08 -0300

    Add files via upload

---
+++ main (10).c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char saudacao[20] = "Olá, ";
+    char nome[20];
+    
+    printf("Digite o seu nome: ");
+    scanf("%s", nome);
+    
+    strcat(saudacao, nome);
+    printf("%s!", saudacao);
+    
+    return 0;
+}
